300a2024ed4d65e5f1427ad21fb4c8d4d3587180
Viktor Filippov <jackal3166@gmail.com>
2023-10-30 14:30:05 +0200

    Create About

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,63 @@
+import styled from 'styled-components';
+
 const About = () => {
-	return <h1>About</h1>;
+	return (
+		<Wrapper>
+			<h3>About Us</h3>
+			<p>
+				Welcome to LiquidLab - your virtual bar designed to immerse you
+				in the fascinating world of cocktail recipes and culinary
+				experiments. At LiquidLab, we're passionately dedicated to
+				curating and presenting the best recipes, aiming to provide you
+				with unforgettable experiences and inspiration in the art of
+				mixology.
+			</p>
+			<h3>Our Mission</h3>
+			<p>
+				LiquidLab is a project born out of a love for cocktail culture.
+				Our primary objective is to offer you a wide array of exquisite
+				drink recipes that will allow you to bring your boldest ideas to
+				life and relish the unique taste of each cocktail.
+			</p>
+			<h3>What We Offer</h3>
+			<p>
+				Our platform presents a multitude of cocktail recipes of varying
+				complexity, from classic masterpieces to new, original
+				combinations of ingredients. You have the opportunity to easily
+				find and select the cocktail that aligns with your taste
+				preferences, fitting any occasion - whether it's a party, a
+				romantic date, or simply an evening in a cozy setting.
+			</p>
+			<h3>What Sets Us Apart</h3>
+			<p>
+				LiquidLab is not just a collection of recipes; it's a source of
+				inspiration for those who appreciate quality and original
+				drinks. We consistently update our collection, adding new and
+				unique cocktail ideas, ensuring that every user can feel like a
+				true mixologist.
+			</p>
+			<p>
+				Immerse yourself in a world of amazing aromas and flavors with
+				LiquidLab - a place where everyone can become a creator of
+				splendid cocktails!
+			</p>
+		</Wrapper>
+	);
 };
+
+const Wrapper = styled.div`
+	padding: 1.5rem;
+    max-width: var(--max-width);
+    text-align: center;
+    margin: 0 auto;
+
+	p {
+		line-height: 2;
+		color: var(--grey-500);
+		margin-top: 1.5rem;
+		margin-bottom: 1.5rem;
+        text-align: start;
+	}
+`;
 
 export default About;
