4d3a88534e519673761d2a454ea3fe4454bbb117
Viktor Filippov <jackal3166@gmail.com>
2023-10-30 10:26:22 +0200

    Add router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+import About from './pages/About';
+import Cocktail from './pages/Cocktail';
+import HomeLayout from './pages/HomeLayout';
+import Landing from './pages/Landing';
+import Newsletter from './pages/Newsletter';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <HomeLayout />,
+		errorElement: <Error />,
+		children: [
+			{ index: true, element: <Landing /> },
+			{ path: 'cocktail', element: <Cocktail /> },
+			{ path: 'newsletter', element: <Newsletter /> },
+			{ path: 'about', element: <About /> },
+		],
+	},
+]);
+
 const App = () => {
-	return <h2>liquid-lab</h2>;
+	return <RouterProvider router={router} />;
 };
 
 export default App;
